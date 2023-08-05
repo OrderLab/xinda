fc8cef9fa94865e32de7fdfd8453504dd9161ce5
Ruiming Lu <ruimingl@umich.edu>
2023-08-04 21:21:50 -0400

    no available diskspace on razor14, running on other machines

--- razor-scripts/node_restart/run_razor14.sh
+++ razor-scripts/node_restart/run_razor14.sh
@@ -20,20 +20,20 @@ meta_log_loc=${data_dir}/${log_dir2}/wkl${1}_meta.log
 main_bash_loc=/data/ruiming/xinda/razor-scripts/node_restart/0803.sh 
 # status_ary=(slow)
 
-duration_ary=(5)
-start_time_ary=(25 26 27 28 29 30 31 32 33 34 35 36 37 38 39)
-for duration in ${duration_ary[@]}; do
-    for start_time in ${start_time_ary[@]}; do
-        # echo restart-slow3-dur${duration}-${start_time}-$((start_time+duration))
-        echo "## [$(date +%s%N), $(date +"%H:%M:%S")] Sourcing cas1 restart-slow3-dur${duration}-${start_time}-$((start_time+duration)).sh now" >> $meta_log_loc
-        bash $main_bash_loc $1 10000 10000000 slow $2 restart-slow3-dur${duration}-${start_time}-$((start_time+duration))
-        echo "## [$(date +%s%N), $(date +"%H:%M:%S")] Sourcing cas1 norestart-slow3-dur${duration}-${start_time}-$((start_time+duration)).sh now" >> $meta_log_loc
-        bash $main_bash_loc $1 10000 10000000 slow $2 norestart-slow3-dur${duration}-${start_time}-$((start_time+duration))
-    done
-done
+# duration_ary=(5)
+# start_time_ary=(25 26 27 28 29 30 31 32 33 34 35 36 37 38 39)
+# for duration in ${duration_ary[@]}; do
+#     for start_time in ${start_time_ary[@]}; do
+#         # echo restart-slow3-dur${duration}-${start_time}-$((start_time+duration))
+#         echo "## [$(date +%s%N), $(date +"%H:%M:%S")] Sourcing cas1 restart-slow3-dur${duration}-${start_time}-$((start_time+duration)).sh now" >> $meta_log_loc
+#         bash $main_bash_loc $1 10000 10000000 slow $2 restart-slow3-dur${duration}-${start_time}-$((start_time+duration))
+#         echo "## [$(date +%s%N), $(date +"%H:%M:%S")] Sourcing cas1 norestart-slow3-dur${duration}-${start_time}-$((start_time+duration)).sh now" >> $meta_log_loc
+#         bash $main_bash_loc $1 10000 10000000 slow $2 norestart-slow3-dur${duration}-${start_time}-$((start_time+duration))
+#     done
+# done
 
 duration_ary=(10)
-start_time_ary=(0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39)
+start_time_ary=(13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39)
 for duration in ${duration_ary[@]}; do
     for start_time in ${start_time_ary[@]}; do
         # echo restart-slow3-dur${duration}-${start_time}-$((start_time+duration))
