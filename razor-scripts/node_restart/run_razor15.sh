5f2b3a852eeab56bbc0dd7a70bf35e3ed8eb76f3
Ruiming Lu <ruimingl@umich.edu>
2023-08-07 13:09:54 -0400

    running duration:50-120 on razors 14 15 16

--- razor-scripts/node_restart/run_razor15.sh
+++ razor-scripts/node_restart/run_razor15.sh
@@ -1,4 +1,4 @@
-###################### /data/ruiming/xinda/razor-scripts/node_restart/run_razor15.sh ######################
+###################### /data/ruiming/xinda/razor-scripts/node_restart/run_razor17.sh ######################
 # $1: workload name
 # $2: location (cas1)
 function create_dir_if_not_exist() {
@@ -20,14 +20,18 @@ meta_log_loc=${data_dir}/${log_dir2}/wkl${1}_meta.log
 main_bash_loc=/data/ruiming/xinda/razor-scripts/node_restart/0803.sh 
 # status_ary=(slow)
 
-duration_ary=(15 20)
+duration_ary=(70 80)
 start_time_ary=(0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39)
 for duration in ${duration_ary[@]}; do
     for start_time in ${start_time_ary[@]}; do
+        end_time=$((start_time+duration))
+        if [ $end_time -ge 120 ]; then
+            end_time=120
+        fi
         # echo restart-slow3-dur${duration}-${start_time}-$((start_time+duration))
-        echo "## [$(date +%s%N), $(date +"%H:%M:%S")] Sourcing cas1 restart-slow3-dur${duration}-${start_time}-$((start_time+duration)).sh now" >> $meta_log_loc
-        bash $main_bash_loc $1 10000 10000000 slow $2 restart-slow3-dur${duration}-${start_time}-$((start_time+duration)) slow3
-        echo "## [$(date +%s%N), $(date +"%H:%M:%S")] Sourcing cas1 norestart-slow3-dur${duration}-${start_time}-$((start_time+duration)).sh now" >> $meta_log_loc
-        bash $main_bash_loc $1 10000 10000000 slow $2 norestart-slow3-dur${duration}-${start_time}-$((start_time+duration)) slow3
+        echo "## [$(date +%s%N), $(date +"%H:%M:%S")] Sourcing $2 restart-slow3-dur${duration}-${start_time}-${end_time}.sh now" >> $meta_log_loc
+        bash $main_bash_loc $1 10000 10000000 slow $2 restart-slow3-dur${duration}-${start_time}-${end_time} slow3
+        echo "## [$(date +%s%N), $(date +"%H:%M:%S")] Sourcing $2 norestart-slow3-dur${duration}-${start_time}-${end_time}.sh now" >> $meta_log_loc
+        bash $main_bash_loc $1 10000 10000000 slow $2 norestart-slow3-dur${duration}-${start_time}-${end_time} slow3
     done
 done
