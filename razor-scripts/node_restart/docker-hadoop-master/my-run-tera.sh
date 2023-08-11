183a32adf3221879f2513b551acecb44a0eacc9c
Ruiming Lu <ruimingl@umich.edu>
2023-08-10 23:02:15 -0400

    add nodemanagers

--- razor-scripts/node_restart/docker-hadoop-master/my-run-tera.sh
+++ razor-scripts/node_restart/docker-hadoop-master/my-run-tera.sh
@@ -1,4 +1,6 @@
 input_dir=/tmp/tera-input
 output_dir=/tmp/tera-output
+docker exec -it namenode mkdir $input_dir
+docker exec -it namenode mkdir $output_dir
 docker exec -it namenode yarn jar /opt/hadoop-3.2.1/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.2.1.jar teragen 1000000 $input_dir
 docker exec -it namenode yarn jar /opt/hadoop-3.2.1/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.2.1.jar terasort $input_dir $output_dir
