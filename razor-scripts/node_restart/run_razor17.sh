0671cd25ab3f7d5422ee2b3cd4fd51d7bed53221
Ruiming Lu <ruimingl@umich.edu>
2023-08-13 16:13:06 -0400

    add razor19

--- razor-scripts/node_restart/run_razor17.sh
+++ razor-scripts/node_restart/run_razor17.sh
@@ -1,4 +1,4 @@
-###################### /data/ruiming/xinda/razor-scripts/node_restart/run_razor16.sh ######################
+###################### /data/ruiming/xinda/razor-scripts/node_restart/run_razor17.sh ######################
 function create_dir_if_not_exist() {
     if [ ! -d $1 ]; then
         mkdir $1
@@ -22,20 +22,54 @@ iteration_123=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
 iteration_4=(76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100)
 iteration_1=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25)
 iteration_234=(26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100)
+# razor14
+iteration_1_60=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60)
+# razor15
+iteration_61_100=(61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100)
+iteration_1_20=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+# razor16
+iteration_21_80=(21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80)
+# razor17
+iteration_81_100=(81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100)
+iteration_1_40=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40)
+# razor19
+iteration_41_100=(41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100)
 
+## Setup 10 (81-100)
+duration=30
+location=cas1
+severity=slow6
+wkl=d
+start_time=30
+setup=setup10-81-100
+log_dir2=${log_dir1}/${setup}
+create_dir_if_not_exist $log_dir2
+meta_log_loc=${data_dir}/${log_dir2}/wkla_meta.log
 
-## Setup 11 (234)
+for iter in ${iteration_81_100[@]}; do
+    end_time=$((start_time+duration))
+    if [ $end_time -ge 120 ]; then
+        end_time=120
+    fi
+    # echo restart-slow3-dur${duration}-${start_time}-$((start_time+duration))
+    echo "## [$(date +%s%N), $(date +"%H:%M:%S")] Sourcing ${iter}/${setup} $location restart-${severity}-dur${duration}-${start_time}-${end_time}.sh now" >> $meta_log_loc
+    bash $main_bash_loc $wkl 10000 10000000 slow $location restart-${severity}-dur${duration}-${start_time}-${end_time} $severity $iter $setup
+    echo "## [$(date +%s%N), $(date +"%H:%M:%S")] Sourcing ${iter}/${setup} $location norestart-${severity}-dur${duration}-${start_time}-${end_time}.sh now" >> $meta_log_loc
+    bash $main_bash_loc $wkl 10000 10000000 slow $location norestart-${severity}-dur${duration}-${start_time}-${end_time} $severity $iter $setup
+done
+
+## Setup 11 (1-40)
 duration=30
 location=cas1
 severity=slow6
 wkl=e
 start_time=30
-setup=setup11-234
+setup=setup11-1-40
 log_dir2=${log_dir1}/${setup}
 create_dir_if_not_exist $log_dir2
 meta_log_loc=${data_dir}/${log_dir2}/wkla_meta.log
 
-for iter in ${iteration_234[@]}; do
+for iter in ${iteration_1_40[@]}; do
     end_time=$((start_time+duration))
     if [ $end_time -ge 120 ]; then
         end_time=120
