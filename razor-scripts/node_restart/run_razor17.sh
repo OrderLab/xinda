8c1ce691d0c4bcded83472a7ec894a11a45c9b2f
Ruiming Lu <ruimingl@umich.edu>
2023-08-13 15:53:57 -0400

    running setups 10-11-12

--- razor-scripts/node_restart/run_razor17.sh
+++ razor-scripts/node_restart/run_razor17.sh
@@ -1,4 +1,4 @@
-###################### /data/ruiming/xinda/razor-scripts/node_restart/run_razor17.sh ######################
+###################### /data/ruiming/xinda/razor-scripts/node_restart/run_razor16.sh ######################
 function create_dir_if_not_exist() {
     if [ ! -d $1 ]; then
         mkdir $1
@@ -18,19 +18,24 @@ main_bash_loc=/data/ruiming/xinda/razor-scripts/node_restart/0810.sh
 iteration_1st_half=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50)
 iteration_2nd_half=(51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100)
 iteration_full=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100)
+iteration_123=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 )
+iteration_4=(76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100)
+iteration_1=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25)
+iteration_234=(26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100)
 
-## Setup 8 (2nd-half)
+
+## Setup 11 (234)
 duration=30
 location=cas1
 severity=slow6
-wkl=b
+wkl=e
 start_time=30
-setup=setup8-2nd-half
+setup=setup11-234
 log_dir2=${log_dir1}/${setup}
 create_dir_if_not_exist $log_dir2
 meta_log_loc=${data_dir}/${log_dir2}/wkla_meta.log
 
-for iter in ${iteration_2nd_half[@]}; do
+for iter in ${iteration_234[@]}; do
     end_time=$((start_time+duration))
     if [ $end_time -ge 120 ]; then
         end_time=120
@@ -41,3 +46,5 @@ for iter in ${iteration_2nd_half[@]}; do
     echo "## [$(date +%s%N), $(date +"%H:%M:%S")] Sourcing ${iter}/${setup} $location norestart-${severity}-dur${duration}-${start_time}-${end_time}.sh now" >> $meta_log_loc
     bash $main_bash_loc $wkl 10000 10000000 slow $location norestart-${severity}-dur${duration}-${start_time}-${end_time} $severity $iter $setup
 done
+
+
