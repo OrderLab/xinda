b0a88a9527373b79eebd5aed504ec2126ae55ce7
Ruiming Lu <ruimingl@umich.edu>
2023-08-27 21:08:12 -0400

    conf hbase

--- razor-scripts/node_restart/init_hbase.sh
+++ razor-scripts/node_restart/init_hbase.sh
@@ -2,5 +2,5 @@
 # create 'usertable', 'family', {SPLITS => (1..n_splits).map {|i| 'user#{1000+i*(9999-1000)/n_splits}'}}" > /tmp/init_hbase.sh
 # n_splits = 200
 # create 'usertable', 'family', {SPLITS => (1..n_splits).map {|i| 'user#{1000+i*(9999-1000)/n_splits}'}}
-echo "n_splits = 200" | ./opt/hbase-1.2.6/bin/hbase shell -n
-echo "create 'usertable', 'family', {SPLITS => (1..n_splits).map {|i| 'user#{1000+i*(9999-1000)/n_splits}'}}" | ./opt/hbase-1.2.6/bin/hbase shell -n
+echo "n_splits = 200" | ./opt/hbase-1.2.6/bin/hbase shell
+echo "create 'ssss', 'family', {SPLITS => (1..n_splits).map {|i| 'user#{1000+i*(9999-1000)/n_splits}'}}" | ./opt/hbase-1.2.6/bin/hbase shell
