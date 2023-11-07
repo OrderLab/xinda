2e9096e90bf28bf67013388e17158fb14f378537
Ruiming Lu <ruimingl@umich.edu>
2023-11-06 20:19:24 -0500

    housecleaning in crdb

--- tools/docker-crdb/sql.sh
+++
@@ -1,2 +0,0 @@
-roach_id=${1:-1}
-docker exec -it roach0 ./cockroach sql --host=roach$roach_id:26257 --insecure
--- tools/docker-crdb/up.sh
+++
@@ -1,3 +0,0 @@
-docker-compose up -d
-sleep 1
-docker exec -it roach0 ./cockroach --host=roach1:26357 init --insecure
