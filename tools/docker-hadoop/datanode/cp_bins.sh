d7f1726009c22eaba152f04d3324298fd8f811e8
yunchi <13524919136@163.com>
2023-11-18 21:13:41 -0500

    jacoco

---
+++ tools/docker-hadoop/datanode/cp_bins.sh
@@ -0,0 +1,8 @@
+cp /home/yunchi/hadoop-3.2.1/hadoop-common-project/hadoop-common/src/main/bin/hadoop jacoco-bins/
+cp /home/yunchi/hadoop-3.2.1/hadoop-common-project/hadoop-common/src/main/bin/hadoop.cmd jacoco-bins/
+cp /home/yunchi/hadoop-3.2.1/hadoop-hdfs-project/hadoop-hdfs/src/main/bin/hdfs jacoco-bins/
+cp /home/yunchi/hadoop-3.2.1/hadoop-hdfs-project/hadoop-hdfs/src/main/bin/hdfs.cmd jacoco-bins/
+cp /home/yunchi/hadoop-3.2.1/hadoop-mapreduce-project/bin/mapred jacoco-bins/
+cp /home/yunchi/hadoop-3.2.1/hadoop-mapreduce-project/bin/mapred.cmd jacoco-bins/
+cp /home/yunchi/hadoop-3.2.1/hadoop-yarn-project/hadoop-yarn/bin/yarn jacoco-bins/
+cp /home/yunchi/hadoop-3.2.1/hadoop-yarn-project/hadoop-yarn/bin/yarn.cmd jacoco-bins/
