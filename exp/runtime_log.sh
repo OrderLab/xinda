d266a3c45cf86a0a3c2bbd59281908e2a5dfbee3
Ruiming Lu <ruimingl@umich.edu>
2024-02-06 22:57:39 -0500

    exp: check runtime status for cloudlab jobs

--- exp/runtime_log.sh
+++ exp/runtime_log.sh
@@ -8,6 +8,11 @@ mkdir -p logs
 # Loop through each host
 for i in "${!hosts[@]}"; do
     host=${hosts[$i]}
-    echo "$host [$(($i+1))/${#hosts[@]}]"
+    echo -n "$host [$(($i+1))/${#hosts[@]}]"
     ssh ${username}@$host cat "~/workdir/xinda/test_scripts/RQ1_1/*.log" > logs/${host}.log
+    if grep -iq "exception" "logs/${host}.log"; then
+        echo " [EXCEPTION]"
+    else
+        echo " [OK]"
+    fi
 done
