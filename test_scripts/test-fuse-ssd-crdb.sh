e8c150fae6fd7730d5a15aaa23337171b1c3d98d
Ruiming Lu <ruimingl@umich.edu>
2024-02-16 16:09:23 -0500

    test_scripts for running charybdefs (fuse-ssd) but no faults injected

---
+++ test_scripts/test-fuse-ssd-crdb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+location=("roach1")
+iter=(1 2 3 4 5 6 7 8 9 10)
+
+for loc in "${location[@]}"
+do
+    for it in "${iter[@]}"
+    do
+        echo "## [$(date +%s%N), $(date +"%Y-%m-%d %H:%M:%S %Z utc%z"), BEGIN] loc:$loc iter:$it" >> /users/rmlu/workdir/meta.log
+        python3 /users/rmlu/workdir/xinda/main.py --sys_name crdb --data_dir charybdefs-baseline-ssd --fault_type fs --fault_location $loc --fault_duration -1 --fault_severity 10000 --fault_start_time 60 --bench_exec_time 150 --ycsb_wkl a --benchmark ycsb --iter $it --charybdefs_mount_dir /var/lib/docker/cfs_mount/tmp
+        echo "## [$(date +%s%N), $(date +"%Y-%m-%d %H:%M:%S %Z utc%z"), END]" >> /users/rmlu/workdir/meta.log
+    done
+done
