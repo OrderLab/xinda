10d1a26900792d3d519edf2e18ed2cf4532f8393
Ruiming Lu <ruiming_lu@outlook.com>
2024-09-05 14:25:20 +0800

    (exp) hbase-finetune-paper (cherrypick some combinations)

---
+++ exp/hbase-finetune-paper/load_balance.py
@@ -0,0 +1,16 @@
+num_nodes = 16
+script = './scripts/finetune.sh'
+
+# Read the script file
+with open(script, 'r') as file:
+    lines = file.readlines()
+
+# Check if the number of lines matches num_nodes
+if len(lines) != num_nodes:
+    raise ValueError(f"The script should have exactly {num_nodes} lines, but it has {len(lines)} lines.")
+
+# Iterate through the lines and write each to a corresponding job file
+for i in range(1, num_nodes + 1):
+    node_file = f'exp/node{i}.job'
+    with open(node_file, 'w') as file:
+        file.write(lines[i - 1])
---
+++ exp/hbase-finetune-paper/runtime_log.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# List of all hosts
+hosts=( $(cat ./hosts | awk '{print $1}') )
+username="rmlu"
+mkdir -p logs
+
+# Loop through each host
+for i in "${!hosts[@]}"; do
+    host=${hosts[$i]}
+    echo -n "$host [$(($i+1))/${#hosts[@]}]"
+    ssh ${username}@$host cat "~/workdir/hbase-pipeline/finetune/*.log" > logs/${host}.log
+    if ! grep -ie "error" -ie "exception" "logs/${host}.log"; then
+        echo -n " [OK]"
+    fi
+    num_end=$(cat logs/${host}.log | grep END | wc -l)
+    echo " [Done: $num_end]"
+done
